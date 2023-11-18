1dd9ce23cbe6ada2435e12e328d3302332e95737
everlast1ngw1nter <at07012004@gmail.com>
2023-11-18 18:20:20 +0500

    project3

--- src/main/java/edu/project3/LogParser.java
+++ src/main/java/edu/project3/LogParser.java
@@ -2,6 +2,7 @@ package edu.project3;
 
 import edu.project3.reportmaker.AdocReportMaker;
 import edu.project3.reportmaker.MarkdownReportMaker;
+import edu.project3.reportmaker.ReportFormat;
 import edu.project3.statistic.AverageAnswerCounter;
 import edu.project3.statistic.LogTotalCounter;
 import edu.project3.statistic.MostFrequentDay;
@@ -16,7 +17,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import static java.net.http.HttpClient.newHttpClient;
@@ -24,7 +28,20 @@ import static java.net.http.HttpClient.newHttpClient;
 
 public class LogParser {
 
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+    public static void main(String[] args)
+            throws URISyntaxException, IOException, InterruptedException {
+        Path path = null;
+        LocalDate from = null;
+        LocalDate to = null;
+        var reportFormat = ReportFormat.MARKDOWN;
+        for (var i = 0; i < args.length; i = i + 2) {
+            switch (args[i]) {
+                case "--path" -> path = Path.of(args[i + 1]);
+                case "--from" -> from = LocalDate.parse(args[i + 1], DateTimeFormatter.ISO_LOCAL_DATE);
+                case "--to" -> to = LocalDate.parse(args[i + 1], DateTimeFormatter.ISO_LOCAL_DATE);
+                case "--format" -> reportFormat = getReportFormat(args[i + 1]);
+            }
+        }
         var request = HttpRequest.newBuilder()
                 .uri(new URI("https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs"))
                 .GET()
@@ -48,5 +65,12 @@ public class LogParser {
                 System.out.println(elem);
             }
         }
+    }
+
+    private static ReportFormat getReportFormat(String stringFormat) {
+        return switch (stringFormat) {
+            case "adoc" -> ReportFormat.ADOC;
+            default -> ReportFormat.MARKDOWN;
+        };
     }
 }
