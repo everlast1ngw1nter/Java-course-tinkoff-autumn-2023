8822331e59f6ad53c4d2449f47c8af9ec283551c
everlast1ngw1nter <at07012004@gmail.com>
2023-11-26 13:29:48 +0500

    hw7

--- src/main/java/edu/hw7/Task4.java
+++ src/main/java/edu/hw7/Task4.java
@@ -1,59 +1,67 @@
 package edu.hw7;
 
+import java.util.Collections;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 public class Task4 {
+    
+    private static final double r = 1;
 
-    public static void singleThreadPiCounter() {
-        long startTime = System.nanoTime();
-
+    public static void singleThreadPiCounter(int totalSimulations) {
+        var startTime = System.nanoTime();
         var circleCount = 0;
         var totalCount = 0;
         var rnd = new Random();
-        for (var i = 0; i < 100000000; i++) {
-            if (isInCircle(rnd.nextDouble(0, 1), rnd.nextDouble(0, 1), 1 )) {
+        for (var i = 0; i < totalSimulations; i++) {
+            if (isInCircle(rnd.nextDouble(0, 1),
+                    rnd.nextDouble(0, 1), r )) {
                 circleCount++;
             }
             totalCount++;
         }
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
+        var endTime = System.nanoTime();
+        var duration = endTime - startTime;
         System.out.println(((double) circleCount / totalCount) * 4);
         System.out.println(duration);
     }
 
-    public static void multiThreadPiCounter() throws InterruptedException {
-        var startTime = System.nanoTime();
-        var threads = 2;
-        var totalSimulations = 100000000;
+    public static void multiThreadPiCounter(int threads, int totalSimulations)
+            throws InterruptedException, ExecutionException {
         var perEachThread = totalSimulations / threads;
-        var circleCount = new AtomicInteger();
-        var totalCount = new AtomicInteger();
-        Runnable runnable = () -> {
+        var circleCount = 0;
+        var totalCount = 0;
+        
+        var startTime = System.nanoTime();
+        Callable<ThreadSimulationResult> runnable = () -> {
+            var threadCircleCount = 0;
+            var threadTotalCount = 0;
             var currRnd = ThreadLocalRandom.current();
             for (var i = 0; i < perEachThread; i++) {
                 if (isInCircle(currRnd.nextDouble(0, 1),
-                        currRnd.nextDouble(0, 1), 1 )) {
-                    circleCount.getAndIncrement();
+                        currRnd.nextDouble(0, 1), r )) {
+                    threadCircleCount++;
                 }
-                totalCount.getAndIncrement();
+                threadTotalCount++;
             }
+            return new ThreadSimulationResult(threadCircleCount, threadTotalCount);
         };
-        var tasks = Stream.generate(() -> new Thread(runnable))
-                .limit(threads)
-                .toList();
-        for (var task : tasks) {
-            task.start();
+
+        try (ExecutorService service = Executors.newFixedThreadPool(threads)) {
+            var futures = service.invokeAll(Collections.nCopies(5, runnable).stream().toList());
+            for (var future : futures) {
+                circleCount += future.get().circleCount;
+                totalCount += future.get().totalCount;
+            }
         }
-        for (var task : tasks) {
-            task.join();
-        }
+
         var endTime = System.nanoTime();
         var duration = endTime - startTime;
-        System.out.println(((double) circleCount.get() / totalCount.get()) * 4);
+        System.out.println(((double) circleCount / totalCount) * 4);
         System.out.println(duration);
     }
 
@@ -61,9 +69,13 @@ public class Task4 {
         return x * x + y * y <= r * r;
     }
 
+    private record ThreadSimulationResult(int circleCount, int totalCount) {}
+
     public static void main(String[] args)
-            throws InterruptedException {
-        multiThreadPiCounter();
-        singleThreadPiCounter();
+            throws InterruptedException, ExecutionException {
+        var totalSimulations = 100000000;
+        var threads = 6;
+        multiThreadPiCounter(threads, totalSimulations);
+        singleThreadPiCounter(totalSimulations);
     }
 }
