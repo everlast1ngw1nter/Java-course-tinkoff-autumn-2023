f777e95d1340212c2460af02486d67307faf99d8
everlast1ngw1nter <at07012004@gmail.com>
2023-11-27 17:54:54 +0500

    hw6

--- src/main/java/edu/hw6/Task2.java
+++ src/main/java/edu/hw6/Task2.java
@@ -4,30 +4,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 
 public class Task2 {
 
     private Task2() {
-
     }
 
     public static void cloneFile(Path path) throws IOException {
-        var copies = getAllCopies(path);
-        var fileName = path.getFileName();
-        var nextFileIndex = findMinNextIndex(copies);
-        var newPathName = createNewFileName(fileName, nextFileIndex);
+        var newPathName = findNextAvailableName(path);
         var fullNewPath = path.getParent().resolve(newPathName).toFile();
         if (!fullNewPath.createNewFile()) {
             throw new IOException("Cant create file in current directory");
         }
         copyFileUsingChannel(path.toFile(), fullNewPath);
+    }
+
+    private static Path findNextAvailableName(Path fileName) {
+        var currIndex = 1;
+        var newPath = createNewFileName(fileName, currIndex);
+        while (newPath.toFile().exists()) {
+            currIndex++;
+            newPath = createNewFileName(fileName, currIndex);
+        }
+        return newPath;
     }
 
     @SuppressWarnings("MultipleStringLiterals")
@@ -43,59 +43,12 @@ public class Task2 {
         return Path.of(stringPath);
     }
 
-    private static int findMinNextIndex(List<Integer> indexes) {
-        indexes.sort(Comparator.naturalOrder());
-        for (var index = 0; index < indexes.size(); index++) {
-            if (indexes.get(index) != index + 1) {
-                return index + 1;
-            }
-        }
-        return indexes.size() + 1;
-    }
-
     private static void copyFileUsingChannel(File source, File dest) throws IOException {
         try (var inputStream = new FileInputStream(source);
              var outputStream = new FileOutputStream(dest)) {
             var sourceChannel = inputStream.getChannel();
             var destChannel = outputStream.getChannel();
             destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-        }
-    }
-
-    @SuppressWarnings("MultipleStringLiterals")
-    private static List<Integer> getAllCopies(Path path) throws IOException {
-        var allCopies = new ArrayList<Integer>();
-        var fileName = path.getFileName();
-        var nameAndExtension = fileName.toString().split("\\.");
-        var fileNameWithoutExtension = nameAndExtension[0];
-        var fileExtension = nameAndExtension[1];
-        Pattern copyPattern =
-                Pattern.compile(fileNameWithoutExtension + " - копия ?\\(?(\\d+)?\\)?\\." + fileExtension);
-        var siblings = getSiblings(path);
-        for (var elem : siblings) {
-            var name = elem.getFileName().toString();
-            var matcher = copyPattern.matcher(name);
-            if (!matcher.find()) {
-                continue;
-            }
-            var copyNumber = matcher.group(1);
-            if (copyNumber == null) {
-                allCopies.add(1);
-            } else {
-                allCopies.add(Integer.valueOf(matcher.group(1)));
-            }
-        }
-        return allCopies;
-    }
-
-    private static List<Path> getSiblings(Path path) throws IOException {
-        var directory = path.getParent();
-        try (Stream<Path> paths = Files.walk(directory)) {
-            return paths
-                    .skip(1)
-                    .toList();
-        } catch (IOException ex) {
-            throw new IOException();
         }
     }
 }
