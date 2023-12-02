479f937afb72c660e66f5d1fae98191ab63f1aae
everlast1ngw1nter <at07012004@gmail.com>
2023-12-02 15:29:57 +0500

    hw8

--- src/main/java/edu/hw8/Task1Server.java
+++ src/main/java/edu/hw8/Task1Server.java
@@ -9,7 +9,7 @@ import java.net.ServerSocket;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
-public class Server {
+public class Task1Server {
 
     private static final Map<String, String> map = Map.of(
             "личности", "Не переходи на личности там, где их нет",
@@ -17,18 +17,13 @@ public class Server {
             "глупый", "А я тебе говорил, что ты глупый? Так вот, я забираю свои слова обратно... Ты просто бог идиотизма.",
             "интеллект", "Чем ниже интеллект, тем громче оскорбления");
 
-    public static void main(String[] args) throws IOException {
-        var executor = Executors.newFixedThreadPool(4);
-        try (ServerSocket server = new ServerSocket(53)){
+    public static void startServer(int threads) throws IOException {
+
+        try (var server = new ServerSocket(53);
+             var executor = Executors.newFixedThreadPool(threads)) {
             while (true) {
                 var client = server.accept();
                 Runnable runnable = () -> {
-                    System.out.println(Thread.currentThread().getName());
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
                     try (var reader = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
                         var inputString = reader.readLine();
                         var writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
--- src/test/java/edu/hw8/Task1Test.java
+++ src/test/java/edu/hw8/Task1Test.java
@@ -1,4 +1,53 @@
 package edu.hw8;
 
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Task1Test {
+
+    private static final List<String> REQUESTS = List.of(
+            "личности", "интеллект",
+            "оскорбления", "глупый");
+
+    private static final Set<String> POSSIBLE_ANSWERS = Set.of(
+            "Не переходи на личности там, где их нет",
+            "Если твои противники перешли на личные оскорбления, будь уверена — твоя победа не за горами",
+            "А я тебе говорил, что ты глупый? Так вот, я забираю свои слова обратно... Ты просто бог идиотизма.",
+            "Чем ниже интеллект, тем громче оскорбления");
+
+    @Test
+    void answerTest() throws InterruptedException, ExecutionException {
+        var serverThread = new Thread(() -> {
+            try {
+                Task1Server.startServer(4);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        serverThread.start();
+        Thread.sleep(1000);
+        Callable<Void> callable = () -> {
+            var rnd = ThreadLocalRandom.current();
+            var ans = Task1Client.getCausticResponse(REQUESTS.get(rnd.nextInt(4)));
+            assertTrue(POSSIBLE_ANSWERS.contains(ans));
+            return null;
+        };
+        try (var executor = Executors.newFixedThreadPool(8)) {
+            var tasks = Stream.generate(() -> callable)
+                    .limit(64)
+                    .toList();
+            var futures = executor.invokeAll(tasks);
+            for (var future : futures) {
+                future.get();
+            }
+        }
+    }
 }
