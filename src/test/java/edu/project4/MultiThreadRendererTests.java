bcea801fbdd104f7db3d4ae7c8f34f9838594af2
everlast1ngw1nter <at07012004@gmail.com>
2023-12-10 16:55:07 +0500

    project4

---
+++ src/test/java/edu/project4/MultiThreadRendererTests.java
@@ -0,0 +1,21 @@
+package edu.project4;
+
+import edu.project4.render.MultiThreadRenderer;
+import edu.project4.render.SingleThreadRenderer;
+import edu.project4.transformations.Cross;
+import edu.project4.transformations.Exponential;
+import edu.project4.transformations.Linear;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class MultiThreadRendererTests {
+
+    @Test
+    void multiThreadRendererTests() {
+        var rend = new MultiThreadRenderer(new ImageConfig(-1.777, 1.777, -1, 1),  5, 4);
+        var canvas = FractalImage.create(30, 30);
+        var variations = List.of(new Cross(), new Exponential(), new Linear());
+        assertDoesNotThrow(() -> rend.render(canvas, variations, 10000, 10, 5));
+    }
+}
---
+++ src/test/java/edu/project4/SingleThreadRendererTests.java
@@ -0,0 +1,22 @@
+package edu.project4;
+
+import edu.project4.render.SingleThreadRenderer;
+import edu.project4.transformations.Cross;
+import edu.project4.transformations.Exponential;
+import edu.project4.transformations.Linear;
+import edu.project4.transformations.Transformation;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class SingleThreadRendererTests {
+
+    @Test
+    void singleThreadRendererTests() {
+        var rend = new SingleThreadRenderer(new ImageConfig(-1.777, 1.777, -1, 1),  5);
+        var canvas = FractalImage.create(30, 30);
+        var variations = List.of(new Cross(), new Exponential(), new Linear());
+        assertDoesNotThrow(() -> rend.render(canvas, variations, 10000, 10, 5));
+    }
+}
