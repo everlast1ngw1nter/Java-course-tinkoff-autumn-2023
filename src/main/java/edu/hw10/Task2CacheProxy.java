79c62638ca71fb242f00bdab7692edcf145f2b2c
everlast1ngw1nter <at07012004@gmail.com>
2023-12-12 14:10:25 +0500

    hw10

--- src/main/java/edu/hw10/Task2CacheProxy.java
+++ src/main/java/edu/hw10/Task2CacheProxy.java
@@ -1,10 +1,17 @@
 package edu.hw10;
 
+import edu.hw10.Task2CacheAnnotation.Cache;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import edu.hw10.Task2CacheAnnotation.Cache;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Task2CacheProxy {
 
@@ -14,22 +21,59 @@ public class Task2CacheProxy {
         return (T) Proxy.newProxyInstance(loader, interfaces, new CacheInvocationHandler<>(proxiedInstance));
     }
 
-    private static class CacheInvocationHandler <T> implements InvocationHandler {
+    private static class CacheInvocationHandler<T> implements InvocationHandler {
 
-        private final T proxiedInstance;
+        private final T obj;
 
-        public CacheInvocationHandler(T proxiedInstance) {
-            this.proxiedInstance = proxiedInstance;
+        private final Map<Integer, Object> cache;
+
+        private CacheInvocationHandler(T obj) {
+            this.obj = obj;
+            cache = new HashMap<>();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args)
-                throws InvocationTargetException, IllegalAccessException {
+                throws InvocationTargetException, IllegalAccessException, IOException {
             var ann = method.getAnnotation(Cache.class);
-            if (ann != null) {
-                System.out.println("Do cache");
+            if (ann == null) {
+                return method.invoke(obj, args);
             }
-            return method.invoke(proxiedInstance, args);
+            var cached = getIfCached(Arrays.hashCode(args));
+            if (cached != null) {
+                return cached;
+            }
+            var methRes = method.invoke(obj, args);
+            cacheData(methRes, args, ann.persist());
+            return methRes;
+        }
+
+        private void saveLocally(Object res, Integer hash) {
+            cache.put(hash, res);
+        }
+
+        private void saveInMemory(Object res, Object[] args, Integer hash) throws IOException {
+            File file = Path.of(".").resolve(String.valueOf(hash)).toFile();
+            file.createNewFile();
+            try (var writer = new FileWriter(file)) {
+                for (var elem : args) {
+                    writer.write("Arg " + elem.toString() + "\n");
+                }
+                writer.write("Result value " + res.toString());
+                writer.flush();
+            }
+        }
+
+        private Object getIfCached(Integer hash) {
+            return cache.get(hash);
+        }
+
+        private void cacheData(Object res, Object[] args, boolean isPersist) throws IOException {
+            var hash = Arrays.hashCode(args);
+            saveLocally(res, hash);
+            if (isPersist) {
+                saveInMemory(res, args, hash);
+            }
         }
     }
 }
--- src/test/java/edu/hw10/Task2Tests.java
+++ src/test/java/edu/hw10/Task2Tests.java
@@ -9,18 +9,20 @@ public class Task2Tests {
     void proxyTest() {
         FibCalculator inst = new RealFib();
         FibCalculator proxy = Task2CacheProxy.create(inst);
-        proxy.fib(12);
+        for (var i = 0; i < 10; i++) {
+            proxy.fib(12, "a");
+        }
         var a = 1 + 1;
     }
 
     public interface FibCalculator {
-        @Cache(persist = true)
-        long fib(int number);
+        @Cache(persist = false)
+        long fib(int number, String str);
     }
 
     public static class RealFib implements FibCalculator {
         @Override
-        public long fib(int number) {
+        public long fib(int number, String str) {
             return 0;
         }
     }
