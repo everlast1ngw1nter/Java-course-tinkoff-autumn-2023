812da95b6ef384da2325ec24803c1f7fd1f6ea75
everlast1ngw1nter <at07012004@gmail.com>
2023-12-13 16:40:03 +0500

    hw11

--- src/test/java/edu/hw11/Task2AndTests.java
+++ src/test/java/edu/hw11/Task2AndTests.java
@@ -1,25 +1,25 @@
 package edu.hw11;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
-import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.implementation.MethodDelegation;
 import org.junit.jupiter.api.Test;
-import java.lang.reflect.InvocationTargetException;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task2AndTests {
 
     @Test
-    void redefineMethodTest() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    void redefineMethodTest() {
+        ByteBuddyAgent.install();
         new ByteBuddy()
                 .redefine(ArithmeticUtils.class)
                 .method(named("sum"))
-                .intercept(FixedValue.value(0))
+                .intercept(MethodDelegation.to(ArithmeticByteBuddy.class))
                 .make()
                 .load(ArithmeticUtils.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
-
-        assertEquals(ArithmeticUtils.sum(3,8), 0);
+        assertEquals(ArithmeticUtils.sum(3,8), 24);
 
     }
 
