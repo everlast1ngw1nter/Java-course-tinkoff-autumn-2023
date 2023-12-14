7468ad74444888e669442a405722a1632cfbacdb
everlast1ngw1nter <at07012004@gmail.com>
2023-12-15 01:30:52 +0500

    hw11

--- src/main/java/edu/hw11/Task3.java
+++ src/main/java/edu/hw11/Task3.java
@@ -10,12 +10,13 @@ import net.bytebuddy.jar.asm.Label;
 import net.bytebuddy.jar.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import static net.bytebuddy.jar.asm.Opcodes.*;
 
 public class Task3 {
 
-    public static long generateNewClass()
-            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static Method generateNewClass()
+            throws NoSuchMethodException {
         Class<?> newClass = new ByteBuddy()
                 .subclass(Object.class)
                 .name("FibCalculator")
@@ -25,10 +26,7 @@ public class Task3 {
                 .make()
                 .load(Task3.class.getClassLoader())
                 .getLoaded();
-//        var obj = newClass.getConstructor().newInstance();
-        var meth = newClass.getMethod("fib", int.class);
-
-        return (long) meth.invoke(null, 1);
+        return newClass.getMethod("fib", int.class);
     }
 
     public static class SumImplementation implements Implementation {
@@ -62,11 +60,11 @@ public class Task3 {
             mv.visitVarInsn(Opcodes.LSTORE, 3);
             mv.visitInsn(Opcodes.LCONST_0);
             mv.visitVarInsn(Opcodes.LSTORE, 5);
+            mv.visitIincInsn(0, -1);
             Label loopStart = new Label();
             Label loopEnd = new Label();
             mv.visitLabel(loopStart);
             mv.visitFrame(Opcodes.F_FULL, 4, new Object[]{Opcodes.INTEGER, Opcodes.LONG, Opcodes.LONG, Opcodes.LONG}, 0,  null);
-
             mv.visitVarInsn(Opcodes.LLOAD, 3);
             mv.visitVarInsn(Opcodes.LLOAD, 1);
             mv.visitInsn(Opcodes.LADD);
@@ -75,15 +73,11 @@ public class Task3 {
             mv.visitVarInsn(Opcodes.ILOAD, 0);
             mv.visitVarInsn(Opcodes.LLOAD, 5);
             mv.visitVarInsn(Opcodes.LLOAD, 3);
-            mv.visitVarInsn(Opcodes.LSTORE, 3);
             mv.visitVarInsn(Opcodes.LSTORE, 1);
-           // mv.visitFrame(Opcodes.F_FULL, 4, new Object[]{Opcodes.INTEGER, Opcodes.LONG, Opcodes.LONG, Opcodes.LONG}, 2,  new Object[]{Opcodes.INTEGER, Opcodes.INTEGER});
-
-//            mv.visitFrame(Opcodes.F_FULL, 4, new Object[]{Opcodes.INTEGER, Opcodes.LONG, Opcodes.LONG, Opcodes.LONG}, 0,  null);
-            mv.visitJumpInsn(Opcodes.IFEQ, loopStart);
+            mv.visitVarInsn(Opcodes.LSTORE, 3);
+            mv.visitJumpInsn(Opcodes.IFNE, loopStart);
             mv.visitFrame(Opcodes.F_FULL, 4, new Object[]{Opcodes.INTEGER, Opcodes.LONG, Opcodes.LONG, Opcodes.LONG}, 0,  null);
             mv.visitLabel(loopEnd);
-
             mv.visitVarInsn(Opcodes.LLOAD, 5);
             mv.visitInsn(Opcodes.LRETURN);
             mv.visitEnd();
--- src/test/java/edu/hw11/Task3Tests.java
+++ src/test/java/edu/hw11/Task3Tests.java
@@ -1,15 +1,27 @@
 package edu.hw11;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import java.lang.reflect.InvocationTargetException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task3Tests {
 
-    @Test
-    void generateNewClass()
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "2, 1",
+            "3, 2",
+            "4, 3",
+            "5, 5",
+            "6, 8",
+            "25, 75025",
+    })
+    void generateNewClass(int input, long expected)
             throws InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         var res = Task3.generateNewClass();
-        assertEquals(res, 2);
+        var fibRes = res.invoke(null, input);
+        assertEquals(expected, fibRes);
     }
 }
