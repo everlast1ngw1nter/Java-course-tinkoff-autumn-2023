d3d2011e4759e6f9fb0100d4762035a6b221d0de
everlast1ngw1nter <at07012004@gmail.com>
2023-12-15 17:24:17 +0500

    hw11

--- src/test/java/edu/hw11/Task2AndTests.java
+++ src/test/java/edu/hw11/Task2AndTests.java
@@ -1,38 +1,37 @@
 package edu.hw11;
 
 import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
 import org.junit.jupiter.api.Test;
-import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
+import java.lang.reflect.InvocationTargetException;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Task2AndTests {
 
     @Test
-    void redefineMethodTest() {
-        ByteBuddyAgent.install();
-        new ByteBuddy()
-                .redefine(ArithmeticUtils.class)
-                .method(named("sum")
-                        .and(isDeclaredBy(ArithmeticUtils.class)))
-                .intercept(MethodDelegation.to(ArithmeticByteBuddy.class))
+    void redefineMethodTest()
+            throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        var dynamicType = new ByteBuddy()
+                .subclass(ArithmeticUtils.class)
+                .method(named("sum"))
+                .intercept(MethodDelegation.to(new ArithmeticByteBuddy()))
                 .make()
-                .load(getClass().getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
-        assertEquals(ArithmeticUtils.sum(3,8), 24);
+                .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
+                .getLoaded();
+        assertEquals(dynamicType.getDeclaredConstructor().newInstance().sum(3,8), 24);
 
     }
 
-    static class ArithmeticByteBuddy {
-        public static int sum(int a, int b) {
+    public static class ArithmeticByteBuddy {
+        public int sum(int a, int b) {
             return a * b;
         }
     }
 
-    static class ArithmeticUtils {
-        public static int sum(int a, int b) {
+    public static class ArithmeticUtils {
+        public int sum(int a, int b) {
             return a + b;
         }
     }
