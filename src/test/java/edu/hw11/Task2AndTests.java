943b48d452a08b3aae66193ee7da64aa7e3e0947
everlast1ngw1nter <at07012004@gmail.com>
2023-12-15 16:51:13 +0500

    hw11

--- src/test/java/edu/hw11/Task2AndTests.java
+++ src/test/java/edu/hw11/Task2AndTests.java
@@ -2,10 +2,10 @@ package edu.hw11;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.dynamic.VisibilityBridgeStrategy;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
 import org.junit.jupiter.api.Test;
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,9 +15,9 @@ public class Task2AndTests {
     void redefineMethodTest() {
         ByteBuddyAgent.install();
         new ByteBuddy()
-                .with(VisibilityBridgeStrategy.Default.NEVER)
                 .redefine(ArithmeticUtils.class)
-                .method(named("sum"))
+                .method(named("sum")
+                        .and(isDeclaredBy(ArithmeticUtils.class)))
                 .intercept(MethodDelegation.to(ArithmeticByteBuddy.class))
                 .make()
                 .load(getClass().getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
